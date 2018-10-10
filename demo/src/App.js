6b86bb1cd6ce6fd29137d024195fffcbaf1f9d66
Melissa Utomo <mutomo@administrators-MacBook-Pro-2.local>
2018-10-10 11:35:43 -0600

    added state and content in App and added tests for counter

--- demo/src/App.js
+++ demo/src/App.js
@@ -3,10 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <h1>Hello Mars!</h1>
+      <div data-test='component-app'>
+        <h1 data-test='counter-display'>The counter is currently {this.state.counter}</h1>
+        <button data-test='increment-button'>Increment counter</button>
       </div>
     );
   }
